0b09dda8189c486ab710339e195c9b8f75cc15e2
bar371 <33864527+BarCohen1@users.noreply.github.com>
2021-11-20 16:58:53 +0200

    first commit - dataHandler.py

---
+++ DataProcessing/dataHandler.py
@@ -0,0 +1,21 @@
+import os.path
+class DataHandler:
+
+    def __init__(self, input_video_path:str=None,
+                 output_video_path:str=None,
+
+                 ):
+        pass
+
+    def read_video_input(self) -> list:
+        ### TODO --- is the return val a list?
+        pass
+
+    def track_persons_and_crop(self) -> None:
+        pass
+
+    def create_clusters(self):
+        pass
+
+
+
