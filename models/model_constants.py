1a155665e3d90509ea3b21974e5b4e271892b1a8
x3yusk <bar.cohen@siemens.com>
2021-11-22 17:53:47 +0200

    Adding model_constants.py, will be in charge of having all the constants for all sub-modules (by importing as well as holding) model_runner.py --- added support to Data processing actions (NOT TESTED!)

---
+++ models/model_constants.py
@@ -0,0 +1,5 @@
+# model Runner actions constants
+from DataProcessing import dataProcessingConstants
+TRACKING = 'tracking'
+RE_ID = 're-id'
+DATA_PROCESSING_ACTIONS = [dataProcessingConstants.CLUSTER, dataProcessingConstants.TRACK_AND_CROP]
--- models/model_runner.py
+++ models/model_runner.py
@@ -1,15 +1,22 @@
 from argparse import ArgumentParser
 from subprocess import call
+
 import sys
+from model_constants import *
+import DataProcessing.dataFactory
 
 
 def get_args():
     parser = ArgumentParser()
-    parser.add_argument('action', help='what action would you like to run?', choices=['tracking'])
+    parser.add_argument('action', help='what action would you like to run?', choices=[TRACKING, RE_ID, DATA_PROCESSING_ACTIONS])
     parser.add_argument('--config', help='config file')
     parser.add_argument('--input', help='input video file or folder')
     parser.add_argument('--output', help='output video file (mp4 format) or folder')
     parser.add_argument('--checkpoint', help='checkpoint file')
+    parser.add_argument('--device', help='device to run on')
+    parser.add_argument('--k_cluster', help='If running clustering on Data Processing')
+    parser.add_argument('--capture_index', help='If running track and crop on Data Processing')
+    parser.add_argument('--acc_threshold', help='If running track and crop on Data Processing')
     return parser.parse_args()
 
 
@@ -23,15 +30,26 @@ def execute_tracking_action():
 
 
 def validate_tracking_args():
+    # TODO transfer the validation to each sub-module
     assert args.config is not None, 'tracking action requires a `--config` argument'
     assert args.input is not None, 'tracking action requires a `--input` argument'
     assert args.output is not None, 'tracking action requires a `--output` argument'
 
 
 def runner():
-    if args.action == 'tracking':
+    if args.action == TRACKING:
         validate_tracking_args()
         execute_tracking_action()
+
+    elif args.action in DATA_PROCESSING_ACTIONS:
+        validate_tracking_args()
+        DataProcessing.dataFactory.data_factory(action=args.action, video_folder_input_path=args.input,
+                                                output_folder_path=args.output, config=args.config,
+                                                checkpoint=args.checkpoint,
+                                                k_cluster=args.k_cluster,
+                                                acc_threshold=args.acc_treshold,
+                                                capture_index=args.capture_index)
+
     else:
         raise Exception(f'Unsupported action! run model_runner.py -h to see the list of possible actions')
 
