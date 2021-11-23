2c3ecc0164683f6b21ed049f96435f28462ddd9b
Bar <bar.cohen@siemens.com>
2021-11-23 23:19:40 +0200

    Fix calling args in model runner. NOT TESTED

--- models/model_constants.py
+++ models/model_constants.py
@@ -2,4 +2,7 @@
 from DataProcessing import dataProcessingConstants
 TRACKING = 'tracking'
 RE_ID = 're-id'
+
+BASIC_ACTIONS = [TRACKING, RE_ID]
 DATA_PROCESSING_ACTIONS = [dataProcessingConstants.CLUSTER, dataProcessingConstants.TRACK_AND_CROP]
+CHOICES = BASIC_ACTIONS + DATA_PROCESSING_ACTIONS
