eced325d4aa4198e595698164854055c6b134ab8
Bar <bar.cohen@siemens.com>
2021-12-15 09:13:19 +0200

    dataProcessingConstants.py - added ID_TO_NAME and NAME_TO_ID mappings faceClassifer.py - frozen model and dataLoaders faceDetector.py - creating a hist of datasamples

--- FaceDetection/faceClassifer.py
+++ FaceDetection/faceClassifer.py
@@ -1,16 +1,21 @@
+import copy
 import pickle
+import time
 from collections import defaultdict
-
-from PIL import Image
 from sklearn.model_selection import train_test_split
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader
+from torchvision import transforms, utils, datasets, models
+import matplotlib.image as mpimg
 
 from DataProcessing.utils import read_labled_croped_images
+from FaceDetection.faceDetector import FaceDetector
 from FaceDetection.facenet_pytorch import MTCNN, InceptionResnetV1
-from torch import nn
+from torch import nn, optim
 from mtcnn.mtcnn import MTCNN as mtcnn_origin
 from matplotlib import pyplot as plt
 import torch
-
+import numpy as np
 
 class Flatten(torch.nn.Module):
     def __init__(self):
@@ -32,17 +37,18 @@ class normalize(torch.nn.Module):
 class FaceClassifer():
 
     def __init__(self, num_classes):
-        self.model_ft = self._create_Incepction_Resnet_for_finetunning()
         self.num_classes = num_classes
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+        self.model_ft = self._create_Incepction_Resnet_for_finetunning()
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer_ft = optim.SGD(self.model_ft.parameters(), lr=1e-2, momentum=0.9)
+        self.exp_lr_scheduler = lr_scheduler.StepLR(self.optimizer_ft, step_size=7, gamma=0.1)
 
 
     def _create_Incepction_Resnet_for_finetunning(self):
-
         model_ft = InceptionResnetV1(pretrained='vggface2', classify=False)
         layer_list = list(model_ft.children())[-5:] # final layers
-
+        print(layer_list)
         model_ft = torch.nn.Sequential(*list(model_ft.children())[:-5]) # skipping 5 last layers
         for param in model_ft.parameters():
             param.requires_grad = False # we dont want to train the original layers
@@ -51,15 +57,120 @@ class FaceClassifer():
         model_ft.last_linear = nn.Sequential(
             Flatten(),
             nn.Linear(in_features=1792, out_features=512, bias=False),
-            normalize()
-        )
-
-        model_ft.logits = nn.Linear(layer_list[3].in_features, len(self.num_classes))
+            normalize())
+        model_ft.logits = nn.Linear(layer_list[4].in_features, self.num_classes)
         model_ft.softmax = nn.Softmax(dim=1)
         model_ft = model_ft.to(self.device)
-        criterion = nn.CrossEntropyLoss()
-        # Observe that all parameters are being optimized
-        optimizer_ft = optim.SGD(model_ft.parameters(), lr=1e-2, momentum=0.9)
-        # Decay LR by a factor of *gamma* every *step_size* epochs
-        exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+        return model_ft
 
+    def create_data_loaders(self, X, y) -> (DataLoader, DataLoader, DataLoader):
+        # apply_transform  = transforms.Compose([
+        #         transforms.RandomHorizontalFlip(),
+        #         transforms.ToTensor(),
+        #         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        #     ])
+        self.imshow(X[0:2])
+        plt.show()
+        print('showed1')
+        X_train, X_val, y_train, y_val = train_test_split(X,y, test_size = 0.2 , random_state = 1)
+        X_val, X_test, y_val, y_test = train_test_split(X_val,y_val, test_size = 0.5 , random_state = 1)
+
+        self.imshow(X_train[0:2])
+        plt.show()
+        print('showed2')
+        print(type(X_train[0]), X_train[0].shape)
+        # X_train = [apply_transform(np.array(x)) for x in X_train]
+        # X_val = apply_transform(np.array(X_val))
+        # X_test = apply_transform(np.array(X_test))
+
+        dl_train = DataLoader((X_train,y_train), batch_size=8, shuffle=True)
+        dl_val = DataLoader((X_val,y_val), batch_size=8, shuffle=True)
+        dl_test = DataLoader((X_test,y_test), batch_size=8, shuffle=True)
+        return dl_train, dl_val, dl_test
+
+    def train_model(self,dl_train:DataLoader, dl_val:DataLoader, num_epochs=25):
+        dataloaders = {'train': dl_train, 'val':dl_val}
+        since = time.time()
+        FT_losses = []
+        best_model_wts = copy.deepcopy(self.model_ft.state_dict())
+        best_acc = 0.0
+        for epoch in range(num_epochs):
+            print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+            print('-' * 10)
+            # Each epoch has a training and validation phase
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    self.model_ft.train()  # Set self.model to training mode
+                else:
+                    self.model_ft.eval()  # Set self.model to evaluate mode
+                running_loss = 0.0
+                running_corrects = 0
+                # Iterate over data.
+                for inputs, labels in dataloaders[phase]:
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device)
+                    # zero the parameter gradients
+                    self.optimizer_ft.zero_grad()
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = self.model_ft(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = self.criterion(outputs, labels)
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            self.optimizer_ft.step()
+                            self.exp_lr_scheduler.step()
+
+                    FT_losses.append(loss.item())
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                #     running_corrects += torch.sum(preds == labels.data)
+                # epoch_loss = running_loss / dataset_sizes[phase]
+                # epoch_acc = running_corrects.double() /
+                # dataset_sizes[phase]
+            # print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+            #     phase, epoch_loss, epoch_acc))
+            # # deep copy the self.model
+            # if phase == 'val' and epoch_acc > best_acc:
+            #     best_acc = epoch_acc
+            #     best_model_wts = copy.deepcopy(self.model.state_dict())
+
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        print('Best val Acc: {:4f}'.format(best_acc))
+        # load best model weights
+        self.model_ft.load_state_dict(best_model_wts)
+        return self.model_ft, FT_losses
+
+    def imshow(self, inp):
+        """Imshow for Tensor."""
+        plt.figure()
+        f , ax = plt.subplots(len(inp),1)
+        for i, img in enumerate(inp):
+            img = img.permute(1,2,0).int().numpy()
+            ax[i].imshow(img)
+        plt.show()
+
+
+if __name__ == '__main__':
+    # fc = FaceDetector()
+    # fc.filter_out_non_face_corps()
+    # X,y = fc.create_X_y_faces()
+    # pickle.dump(X, open("X.pkl",'wb'))
+    # pickle.dump(y, open("y.pkl",'wb'))
+    # print('done')
+    X = pickle.load(open('X.pkl','rb'))
+    y = pickle.load(open('y.pkl','rb'))
+    # print(X,y)
+    fc = FaceClassifer(21)
+    fc.imshow(X[0:5])
+    plt.show()
+
+    x,y,z = fc.create_data_loaders(X,y)
+    for images, labels in x:
+        print(labels)
+        fc.imshow(images)
+    # fc.imshow(t)
--- FaceDetection/faceDetector.py
+++ FaceDetection/faceDetector.py
@@ -1,40 +1,50 @@
 import pickle
-from collections import defaultdict
-
+from collections import defaultdict, Counter
+import plotly.express as px
 from PIL import Image
+import seaborn as sns
 from sklearn.model_selection import train_test_split
-
+import pandas as pd
+from DataProcessing.dataProcessingConstants import ID_TO_NAME
 from DataProcessing.utils import read_labled_croped_images
 from FaceDetection.facenet_pytorch import MTCNN, InceptionResnetV1
 from mtcnn.mtcnn import MTCNN as mtcnn_origin
 from matplotlib import pyplot as plt
 import torch
 
+import plotly.io as pio
+pio.renderers.default = "browser"
+
 class FaceDetector():
 
-    def __init__(self):
-        self.raw_imgs_dict = read_labled_croped_images('/home/bar_cohen/Data-Shoham/Labeled-Data-Cleaned')
+    def __init__(self, raw_images_path:str=None, faces_data_path:str=None):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+        self.raw_images_path = raw_images_path
+        self.faces_data_path = faces_data_path
         self.mtcnn_detector = mtcnn_origin()
         self.facenet_detecor = MTCNN(margin=40, select_largest=False, post_process=False, device=device, thresholds=[0.8,0.8,0.8])
         self.resnet = InceptionResnetV1(pretrained='vggface2', classify=False)
-        self.high_conf_face_imgs = defaultdict(list)
         self.face_treshold = 0.90
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.high_conf_face_imgs = defaultdict(list)
+        self.filter_out_non_face_corps()
 
-    def filter_out_non_face_corps(self, pickle_path=None) -> None:
+
+    def filter_out_non_face_corps(self) -> None:
         """ Given a set of image crop filter out all images without the faces present"""
-        if pickle_path:
+        if self.faces_data_path:
             print("pickle path to images received, loading...")
-            self.high_conf_face_imgs = pickle.load(open(pickle_path,'rb'))
+            self.high_conf_face_imgs = pickle.load(open(self.faces_data_path,'rb'))
         else:
+            assert self.raw_images_path , 'Pickle to existing face images not found, please input raw images path'
+            raw_imgs_dict = read_labled_croped_images(self.raw_images_path)
 
-            given_num_of_images = sum([len(self.raw_imgs_dict[i]) for i in self.raw_imgs_dict.keys()])
+            print('Number of unique ids', len(raw_imgs_dict.keys()))
+            given_num_of_images = sum([len(raw_imgs_dict[i]) for i in raw_imgs_dict.keys()])
             print(f"Received a total of {given_num_of_images} images")
             counter = 0
-            for id in self.raw_imgs_dict.keys():
-                for img in self.raw_imgs_dict[id]:
+            for id in raw_imgs_dict.keys():
+                for img in raw_imgs_dict[id]:
                     print(f'{counter}/{given_num_of_images}')
                     # plt.imshow(img)
                     # plt.show()
@@ -50,7 +60,6 @@ class FaceDetector():
             print(f'Post filter left with {given_num_of_images_final}')
             pickle.dump(self.high_conf_face_imgs, open('C:\KinderGuardian\FaceDetection\imgs_with_face_highconf.pkl','wb'))
 
-
     def create_X_y_faces(self) -> ([] , []):
         """
         Iterate over high_conf_face_imgs and extract the X images and y labels
@@ -63,36 +72,18 @@ class FaceDetector():
                 y.append(k)
         return X, y
 
-
-    def build_basic_face_classifier(self):
-        X, y = self.create_X_y_faces()
-        X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.1 , random_state = 1)
-        del X, y
-        X_train, y_train = torch.Tensor(X_train), torch.Tensor(y_train)
-        X_test, y_test = torch.Tensor(X_test), torch.Tensor(y_test)
-
-        self.resnet
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def build_samples_hist(self, samples_dict:dict, title:str=None):
+        """Create a Bar plot that represented the number of face samples from every id"""
+        cnt = {ID_TO_NAME[int(k)] : [len(samples_dict[k])] for k in samples_dict.keys()}
+        df = pd.DataFrame.from_dict(cnt)
+        ax= sns.barplot(data=df)
+        ax.set_xticklabels(df.columns, rotation=45)
+        plt.title(title)
+        plt.show()
 
 if __name__ == '__main__':
-    fc = FaceClassifer()
-    fc.filter_out_non_face_corps()
-
-    
-
+    # C:\KinderGuardian\FaceDetection\imgs_with_face_highconf.pkl
+    fc = FaceDetector(raw_images_path='/home/bar_cohen/Data-Shoham/Labeled-Data-Cleaned', faces_data_path='C:\KinderGuardian\FaceDetection\imgs_with_face_highconf.pkl')
+    # read_labled_croped_images(self.raw_images_path)
+    fc.build_samples_hist(fc.high_conf_face_imgs, 'Face Images Hist')
+    fc.build_samples_hist(read_labled_croped_images(fc.raw_images_path), title='Raw Images Hist')
