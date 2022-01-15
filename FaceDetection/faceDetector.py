021c1db6587a3c33c5abe96456dcc631656c431b
bar371 <33864527+BarCohen1@users.noreply.github.com>
2022-01-15 18:31:44 +0200

    faceDetector.py documented and tested, ready.

--- FaceDetection/faceDetector.py
+++ FaceDetection/faceDetector.py
@@ -1,36 +1,34 @@
 import pickle
 from collections import defaultdict, Counter
-from PIL import Image
 import seaborn as sns
-from sklearn.model_selection import train_test_split
 import pandas as pd
 from DataProcessing.dataProcessingConstants import ID_TO_NAME
 from DataProcessing.utils import read_labled_croped_images
 from FaceDetection.facenet_pytorch import MTCNN, InceptionResnetV1
-# from mtcnn.mtcnn import MTCNN as mtcnn_origin
 from matplotlib import pyplot as plt
 import torch
-import torchvision
-
-# import plotly.io as pio
-# pio.renderers.default = "browser"
 
 class FaceDetector():
-
+    """
+    The faceDetector class is in charge of using the MTCNN face detector
+    params:
+    raw_images_path - path to folder containing images for face detection
+    face_data_path - path to folder already containing face images. This will be used in order to put said images into
+    the correct format.
+    """
     def __init__(self, raw_images_path:str=None, faces_data_path:str=None):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.raw_images_path = raw_images_path
         self.faces_data_path = faces_data_path
-        # self.mtcnn_detector = mtcnn_origin()
         self.facenet_detecor = MTCNN(margin=40, select_largest=False, post_process=False, device=device, thresholds=[0.8,0.8,0.8])
-        self.resnet = InceptionResnetV1(pretrained='vggface2', classify=False)
         self.face_treshold = 0.90
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.high_conf_face_imgs = defaultdict(list)
 
 
     def filter_out_non_face_corps(self) -> None:
-        """ Given a set of image crop filter out all images without the faces present"""
+        """ Given a set of image crop filter out all images without the faces present
+        and update the high conf face img member """
         if self.faces_data_path:
             print("pickle path to images received, loading...")
             self.high_conf_face_imgs = pickle.load(open(self.faces_data_path,'rb'))
@@ -45,20 +43,14 @@ class FaceDetector():
             for id in raw_imgs_dict.keys():
                 for img in raw_imgs_dict[id]:
                     print(f'{counter}/{given_num_of_images}')
-                    # plt.imshow(img)
-                    # plt.show()
                     ret = self.facenet_detecor(img)
                     if ret is not None and ret is not ret.numel():
-                        # print(ret.shape)
-                        # plt.imshow(ret.permute(1, 2, 0).int().numpy())
-                        # plt.show()
-                        # input()
                         self.high_conf_face_imgs[id].append(ret)
                     counter += 1
 
             given_num_of_images_final = sum([len(self.high_conf_face_imgs[i]) for i in raw_imgs_dict.keys()])
             print(f'Post filter left with {given_num_of_images_final}')
-            pickle.dump(self.high_conf_face_imgs, open('C:\KinderGuardian\FaceDetection\imgs_with_face_highconf.pkl','wb'))
+            pickle.dump(self.high_conf_face_imgs, open('C:\KinderGuardian\FaceDetection\imgs_with_face_highconf_1.pkl','wb'))
 
     def create_X_y_faces(self) -> ([] , []):
         """
@@ -81,10 +73,15 @@ class FaceDetector():
         plt.title(title)
         plt.show()
 
+def main():
+    fd = FaceDetector(raw_images_path='/home/bar_cohen/Data-Shoham/Labeled-Data-Cleaned',
+                      faces_data_path='C:\KinderGuardian\FaceDetection\imgs_with_face_highconf.pkl')
+    fd.filter_out_non_face_corps()
+    X,y = fd.create_X_y_faces()
+    print(len(X), len(y))
+    fd.build_samples_hist(fd.high_conf_face_imgs, 'Face Images Hist')
+    # fd.build_samples_hist(read_labled_croped_images(fd.raw_images_path), title='Raw Images Hist')
+
 if __name__ == '__main__':
-    pass
-    # C:\KinderGuardian\FaceDetection\imgs_with_face_highconf.pkl
-    # fc = FaceDetector(raw_images_path='/home/bar_cohen/Data-Shoham/Labeled-Data-Cleaned', faces_data_path='C:\KinderGuardian\FaceDetection\imgs_with_face_highconf.pkl')
-    # # read_labled_croped_images(self.raw_images_path)
-    # fc.build_samples_hist(fc.high_conf_face_imgs, 'Face Images Hist')
-    # fc.build_samples_hist(read_labled_croped_images(fc.raw_images_path), title='Raw Images Hist')
+    main()
+    print('done')
