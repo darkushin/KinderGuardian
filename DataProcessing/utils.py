6d1e722750c3ae568326f1a1976e8e1b795a5afc
darkushin <61732335+darkushin@users.noreply.github.com>
2022-01-16 15:28:19 +0200

    Face detection (#8)
    * dataFactory.py added assertion for input args , some logging.
    
    dataHandler.py - logging. moving model init outside for function. supports reading mp4 files. Added assertion for empty folders
    
    model_runner.py - fixed income arg -- acc_treshold
    
    * Add Combined ReID and Tracking Model
    
    * supporting both models
    
    * Support convenient dataset creation
    
    * small updates
    
    * Code Review updates
    
    * renamed function call
    
    * Adding ability to read tagged images
    
    * faceClassifer.py - class for face detection and classfication
    currently supports extracting crops with visible face
    
    * Adding faceClassifer.py and faceDetector.py
    faceDetector.py was changed from the name faceClassifer.py
    and is now in charge of face detection and creation of a face relevent dataset.
    faceClassifer.py will hold our face classifer model
    
    * dataProcessingConstants.py - added ID_TO_NAME and NAME_TO_ID mappings
    faceClassifer.py - frozen model and dataLoaders
    faceDetector.py - creating a hist of datasamples
    
    * FaceNet is running. But is it learning?
    
    * Lots of updates to FaceNet
    Currently overfitting
    
    * minor changes in saving pkl data to correc path on faceClassifer.py
    
    * Adapting model_runner.py to work on combo model
    track_and_reid_model.py adding ability to collect data using labeleld and re-id model and the Crop class
    
    * Running FaceDetection + Saving FaceCrop by track run
    
    * Fixed num_of_classes and loaded trained model to faceClassifer obj
    
    * Commented out faceClassifying part for now
    
    * fixed issues with using faceClassifer
    Fixed Crop_Id bug
    
    * Changes to Crop Model
    Fixing issue with crop_id naming when creating crop object
    
    * Fixes to face-id model usage:
    1. using label encoding for the prediction
    2. adding model.eval() prior to inference
    
    * Added video-trim to utils and updated model_runner config example for re-id-and-track
    
    * Moving Crop obj to dataHandler.py
    
    * faceDetector.py documented and tested, ready.
    
    * faceClassifer.py documented and tested, ready.
    label encoder now member of object class
    
    * track_and_reid_model.py.py documented and tested, ready for data prep
    model_runner.py - small cleaning
    
    * Update DataProcessing/dataHandler.py
    
    * PR changes
    
    Co-authored-by: Bar <bar.cohen@siemens.com>
    Co-authored-by: bar371 <33864527+BarCohen1@users.noreply.github.com>

--- DataProcessing/utils.py
+++ DataProcessing/utils.py
@@ -1,4 +1,13 @@
 import os
+from collections import defaultdict
+
+from cv2 import imread
+from pathlib import Path
+from tqdm import tqdm
+
+import tempfile
+
+import mmcv
 
 """
 This folder holds functions that can be useful for data handling, such as renaming images etc.
@@ -40,8 +49,49 @@ def remove_images_from_dataset(path, pattern):
             os.remove(os.path.join(path, im))
 
 
+def read_labeled_croped_images(file_dir, file_type='jpg') -> dict:
+    """
+    used to load images from a folder, recursively and returns dict with mapping between
+    each image crop and it's id
+    file-dir: dir to load images from
+    return an ID to images dict.
+    """
+
+    # '/home/bar_cohen/Data-Shoham/Labeled-Data-Cleaned' cur path for labled data
+
+    assert os.path.isdir(file_dir) , 'Read labeled data must get a valid dir path'
+    imgs = defaultdict(list)
+    print('reading imgs from dir...')
+    for img in tqdm(Path(file_dir).rglob(f'*.{file_type}')):
+        img = str(img)
+        if not os.path.isfile(img):
+            continue
+        im_path = os.path.split(img)[1]
+        img_id = im_path[:4] # xxxx id format
+        imgs[img_id].append(imread(img))
+    return imgs
+
+
+def trim_video(input_path, output_path, limit):
+    """
+    Given a path to a video and the number of frames that should be taken from it, trim the video to the first `limit` frames. Saves the output to the `output_path` location.
+    """
+    imgs = mmcv.VideoReader(input_path)
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_path = temp_dir.name
+    fps = int(imgs.fps)
+    print('Starting to save imgs:')
+    for i, img in enumerate(imgs):
+        if not i % 100:
+            print(f'{i} frames done.')
+        if i > limit:
+            break
+        mmcv.imwrite(img, f'{temp_path}/{i:03d}.png')
+    mmcv.frames2video(temp_path, output_path, fps=fps, fourcc='mp4v', filename_tmpl='{:03d}.png')
+    temp_dir.cleanup()
+
+
 if __name__ == '__main__':
-    rename_folders = ['third-query-2.8_test-4.8/bounding_box_train']
-    for folder in rename_folders:
-        remove_images_from_dataset(f'/home/bar_cohen/KinderGuardian/fast-reid/datasets/{folder}', 'f03')
+    trim_video('/home/bar_cohen/Data-Shoham/1.8.21_cam1/videos/IPCamera_20210801095724.avi',
+               '/home/bar_cohen/KinderGuardian/Videos/trimmed_1.8.21-095724.mp4', limit=500)
 
---
+++ FaceDetection/faceDetector.py
@@ -0,0 +1,84 @@
+import pickle
+from collections import defaultdict, Counter
+import seaborn as sns
+import pandas as pd
+from DataProcessing.dataProcessingConstants import ID_TO_NAME
+from DataProcessing.utils import read_labeled_croped_images
+from FaceDetection.facenet_pytorch import MTCNN, InceptionResnetV1
+from matplotlib import pyplot as plt
+import torch
+
+class FaceDetector():
+    """
+    The faceDetector class is in charge of using the MTCNN face detector
+    params:
+    raw_images_path - path to folder containing images for face detection
+    face_data_path - path to folder already containing face images. This will be used in order to put said images into
+    the correct format.
+    """
+    def __init__(self, raw_images_path:str=None, faces_data_path:str=None):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.raw_images_path = raw_images_path
+        self.faces_data_path = faces_data_path
+        self.facenet_detecor = MTCNN(margin=40, select_largest=False, post_process=False, device=device, thresholds=[0.8,0.8,0.8])
+        self.face_treshold = 0.90
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.high_conf_face_imgs = defaultdict(list)
+
+
+    def filter_out_non_face_corps(self) -> None:
+        """ Given a set of image crop filter out all images without the faces present
+        and update the high conf face img member """
+        if self.faces_data_path:
+            print("pickle path to images received, loading...")
+            self.high_conf_face_imgs = pickle.load(open(self.faces_data_path,'rb'))
+        else:
+            assert self.raw_images_path , 'Pickle to existing face images not found, please input raw images path'
+            raw_imgs_dict = read_labeled_croped_images(self.raw_images_path)
+
+            print('Number of unique ids', len(raw_imgs_dict.keys()))
+            given_num_of_images = sum([len(raw_imgs_dict[i]) for i in raw_imgs_dict.keys()])
+            print(f"Received a total of {given_num_of_images} images")
+            counter = 0
+            for id in raw_imgs_dict.keys():
+                for img in raw_imgs_dict[id]:
+                    print(f'{counter}/{given_num_of_images}')
+                    ret = self.facenet_detecor(img)
+                    if ret is not None and ret is not ret.numel():
+                        self.high_conf_face_imgs[id].append(ret)
+                    counter += 1
+
+            given_num_of_images_final = sum([len(self.high_conf_face_imgs[i]) for i in raw_imgs_dict.keys()])
+            print(f'Post filter left with {given_num_of_images_final}')
+            pickle.dump(self.high_conf_face_imgs, open('C:\KinderGuardian\FaceDetection\imgs_with_face_highconf_1.pkl','wb'))
+
+    def create_X_y_faces(self) -> ([] , []):
+        """
+        Iterate over high_conf_face_imgs and extract the X images and y labels
+        """
+        assert self.high_conf_face_imgs , "high conf face images must be non-empty"
+        X, y = [], []
+        for k in self.high_conf_face_imgs.keys():
+            for img in self.high_conf_face_imgs[k]:
+                X.append(img)
+                y.append(k)
+        return X, y
+
+    def build_samples_hist(self, samples_dict:dict, title:str=None):
+        """Create a Bar plot that represented the number of face samples from every id"""
+        cnt = {ID_TO_NAME[int(k)] : [len(samples_dict[k])] for k in samples_dict.keys()}
+        df = pd.DataFrame.from_dict(cnt)
+        ax= sns.barplot(data=df)
+        ax.set_xticklabels(df.columns, rotation=45)
+        plt.title(title)
+        plt.show()
+
+def main():
+    """Simple test of FaceDetector"""
+    fd = FaceDetector(raw_images_path='/home/bar_cohen/Data-Shoham/Labeled-Data-Cleaned',
+                      faces_data_path='C:\KinderGuardian\FaceDetection\imgs_with_face_highconf.pkl')
+    fd.filter_out_non_face_corps()
+    X,y = fd.create_X_y_faces()
+    print(len(X), len(y))
+    fd.build_samples_hist(fd.high_conf_face_imgs, 'Face Images Hist')
+    # fd.build_samples_hist(read_labled_croped_images(fd.raw_images_path), title='Raw Images Hist')
