a1dd4107e3f5022ccfe9dc1df41d04ba76443947
bar371 <31073837+bar371@users.noreply.github.com>
2022-05-27 10:05:37 +0300

    Same Day Gallery Creation Updates (#30)

--- SameDayReid/gallery_creation.py
+++ SameDayReid/gallery_creation.py
@@ -6,39 +6,47 @@ import os
 import torch
 import tqdm
 import numpy as np
-from DataProcessing.dataProcessingConstants import ID_TO_NAME, NAME_TO_ID
+
+from mmtracking.mmtrack.apis import init_model, inference_mot
+from mmtrack.apis import inference_mot, init_model
 from FaceDetection.augmentions import normalize_image
 from FaceDetection.faceClassifer import FaceClassifer
 from FaceDetection.faceDetector import FaceDetector, is_img
-from mmtracking.mmtrack.apis import init_model, inference_mot
-from mmtrack.apis import inference_mot, init_model
 
 TRACKING_CONFIG_PATH = "/home/bar_cohen/KinderGuardian/mmtracking/configs/mot/bytetrack/bytetrack_yolox_x_crowdhuman_mot17-private-half.py"
 TRACKING_CHECKPOINT = "/home/bar_cohen/KinderGuardian/mmtracking/checkpoints/bytetrack_yolox_x_crowdhuman_mot17-private-half_20211218_205500-1985c9f0.pth"
-FACE_CHECKPOINT = "/mnt/raid1/home/bar_cohen/FaceData/checkpoints/FULL_DATA_augs:True_lr:1e-05_0, 4.pth"
+FACE_CHECKPOINT = "/mnt/raid1/home/bar_cohen/FaceData/checkpoints/FULL_DATA_augs:True_lr:0.001_0, 4.pth"
+
+FACE_NET = 'FaceNet'
 class GalleryCreator:
     def __init__(self, gallery_path, label_encoder,
-                 min_face_size=20,
-                 tracker_conf_threshold = 0.98,
+                 min_face_size=50,
+                 tracker_conf_threshold = 0.99,
                  device='cuda:1',
                  track_config=TRACKING_CONFIG_PATH,
                  track_checkpoint=TRACKING_CHECKPOINT,
-                 face_clf_checkpoint=FACE_CHECKPOINT):
+                 ):
+        self.tracking_model = init_model(track_config, track_checkpoint, device=device)
+        self.tracker_conf_threshold = tracker_conf_threshold
         self.gallery_path = gallery_path
-        self.faceDetector = FaceDetector(faces_data_path=None,thresholds=[0.97,0.97,0.97],
+        self.global_video_counter = 0 # counts the unique videos entered for the gallery
+
+        # if face_model == FACE_NET:
+        self.faceDetector = FaceDetector(faces_data_path=None,thresholds=[0.98,0.98,0.98],
                                     keep_all=False,min_face_size=min_face_size,
                                     device=device)
         self.faceClassifer = FaceClassifer(num_classes=21, label_encoder=label_encoder, device=device)
         self.faceClassifer.model_ft.load_state_dict(torch.load(FACE_CHECKPOINT))
         self.faceClassifer.model_ft.eval()
-        self.tracking_model = init_model(track_config, track_checkpoint, device=device)
-        self.tracker_conf_threshold = tracker_conf_threshold
 
-    def add_video_to_gallery(self, video_path:str):
+    def add_video_to_gallery_using_FaceNet(self, video_path:str, skip_every=500):
         imgs = mmcv.VideoReader(video_path)
+        self.global_video_counter += 1
         vid_name = video_path.split('/')[-1][9:-4]
-
+        global_i = 0
         for image_index, img in tqdm.tqdm(enumerate(imgs), total=len(imgs)):
+            if image_index % skip_every != 0:
+                continue
             result = tracking_inference(self.tracking_model, img, image_index,
                                         acc_threshold=float(self.tracker_conf_threshold))
             ids = list(map(int, result['track_results'][0][:, 0]))
@@ -48,23 +56,29 @@ class GalleryCreator:
             crop_candidates_inds = []
             crop_candidates_faces = []
             for i, (id, conf, crop_im) in enumerate(zip(ids, confs, crops_imgs)):
-                face_img = self.faceDetector.facenet_detecor(crop_im)
-                if is_img(face_img):
-                    face_img = normalize_image(face_img)
-                    crop_candidates_faces.append(face_img)
-                    crop_candidates_inds.append(i)
+                try:
+                    face_img = self.faceDetector.facenet_detecor(crop_im)
+                    if is_img(face_img):
+                        face_img = normalize_image(face_img)
+                        crop_candidates_faces.append(face_img)
+                        crop_candidates_inds.append(i)
+                except:
+                    print("OMG WTF")
+                    continue
             if len(crop_candidates_faces) > 0: # some faces where detected
-                preds, _ = self.faceClassifer.predict(torch.stack(crop_candidates_faces))
+                preds, outs = self.faceClassifer.predict(torch.stack(crop_candidates_faces))
                 labels = [self.faceClassifer.le.inverse_transform([int(pred)])[0] for pred in preds]
-                crop_cands = np.array(crops_imgs)[crop_candidates_inds]
-                for i , (crop_im, label) in enumerate(zip(crop_cands, labels)):
-                    crop_name = f'{label:04d}_c1_f{i:07d}_{vid_name}.jpg'
-                    # dir_path = os.path.join(self.gallery_path, ID_TO_NAME[label])
-                    dir_path = self.gallery_path
-                    os.makedirs(dir_path, exist_ok=True)
-                    mmcv.imwrite(np.array(crop_im), os.path.join(dir_path, crop_name))
-
-
+                confidences = [out.argmax() for out in outs]
+                crop_cands = [crop_im for i, crop_im in enumerate(crops_imgs) if i in crop_candidates_inds]
+                for i , (crop_im, label, confidence) in enumerate(zip(crop_cands, labels, confidences)):
+                    if confidence > 0.98:
+                        print(label, confidence)
+                        crop_name = f'{label:04d}_c{self.global_video_counter}_f{global_i:07d}.jpg'
+                        # dir_path = os.path.join(self.gallery_path, ID_TO_NAME[label])
+                        dir_path = self.gallery_path
+                        os.makedirs(dir_path, exist_ok=True)
+                        global_i += 1
+                        mmcv.imwrite(np.array(crop_im), os.path.join(dir_path, crop_name))
 
 def tracking_inference(tracking_model, img, frame_id, acc_threshold=0.98):
     result = inference_mot(tracking_model, img, frame_id=frame_id)
@@ -73,24 +87,13 @@ def tracking_inference(tracking_model, img, frame_id, acc_threshold=0.98):
     return result
 
 
-def collect_faces_from_video(video_path:str) -> []:
-    fd = FaceDetector(faces_data_path=None,thresholds=[0.97,0.97,0.97],keep_all=False,min_face_size=40)
-    imgs = mmcv.VideoReader(video_path)
-    ret = []
-    for img in tqdm.tqdm(imgs):
-        face_img, prob = fd.facenet_detecor(img, return_prob=True)
-        if is_img(face_img):
-            ret.append(face_img[0])
-    return ret
-
-def collect_faces_from_list_of_videos(list_of_videos:list):
-    face_imgs = []
-    for video_path in list_of_videos:
-        face_imgs.extend(collect_faces_from_video(video_path=video_path))
-    return face_imgs
-
 if __name__ == '__main__':
     le = pickle.load(open("/mnt/raid1/home/bar_cohen/FaceData/le.pkl",'rb'))
-    gc = GalleryCreator(gallery_path="/mnt/raid1/home/bar_cohen/OUR_DATASETS/same_day_gallery/",
+    gc = GalleryCreator(gallery_path="/home/bar_cohen/KinderGuardian/fast-reid/datasets/same_day_0808_no_skip/bounding_box_test/",
                         label_encoder=le, device='cuda:1')
-    gc.add_video_to_gallery("/mnt/raid1/home/bar_cohen/trimmed_videos/IPCamera_20210803105422/IPCamera_20210803105422_s0_e501.mp4")
+
+
+    gc.add_video_to_gallery_using_FaceNet("/mnt/raid1/home/bar_cohen/Data-Shoham/8.8.21_cam1/videos/IPCamera_20210808120339.avi", skip_every=1)
+    gc.add_video_to_gallery_using_FaceNet("/mnt/raid1/home/bar_cohen/Data-Shoham/8.8.21_cam1/videos/IPCamera_20210808073000.avi", skip_every=1)
+    gc.add_video_to_gallery_using_FaceNet("/mnt/raid1/home/bar_cohen/Data-Shoham/8.8.21_cam1/videos/IPCamera_20210808082440.avi", skip_every=1)
+    gc.add_video_to_gallery_using_FaceNet("/mnt/raid1/home/bar_cohen/Data-Shoham/8.8.21_cam1/videos/IPCamera_20210808092457.avi", skip_every=1)
